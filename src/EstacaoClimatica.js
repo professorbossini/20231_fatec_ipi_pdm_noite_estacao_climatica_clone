77e26bff7895179d7e97f2a56d0632dbdc4b075d
Rodrigo Bossini <professorbossini@gmail.com>
2023-03-10 20:26:43 -0300

    apostila06(estado): fim da secao 2.5
    - componente EstacaoClimatica criado e utilizado pelo componente App
    - componente App passa partes de seu estado ao EstacaoClimatica via props

---
+++ src/EstacaoClimatica.js
@@ -0,0 +1,45 @@
+// rce
+import React, { Component } from 'react'
+
+export class EstacaoClimatica extends Component {
+  render() {
+    return (
+      <div className="card">
+        <div className="card-body">
+
+          <div
+            className="d-flex align-items-center border rounded mb-2"
+            style={{ height: '6rem' }}>
+            <i className={`fas fa-5x ${this.props.icone}`}></i>
+            <p className="w-75 ms-3 text-center fs-1">{this.props.estacao}</p>
+          </div>
+          <div>
+            <p className="text-center">
+              {
+                this.props.latitude ?
+                  `Coordenadas: ${this.props.latitude}, ${this.props.longitude}. Data: ${this.props.data}.`
+                  : this.props.mensagemDeErro ?
+                    `${this.props.mensagemDeErro}`
+                    :
+                    `Clique no botão para saber a sua estação climática`
+              }
+            </p>
+          </div>
+          <button
+            onClick={this.props.obterLocalizacao}
+            className="btn btn-outline-primary w-100 mt-2">
+            Qual a minha estação
+          </button>
+          {/* button.btn.btn-outline-danger.w-100.mt-2{Desmontar componente} */}
+          <button
+            className="btn btn-outline-danger w-100 mt-2"
+            onClick={() => ReactDOM.unmountComponentAtNode(document.querySelector('#root'))}>
+            Desmontar componente
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default EstacaoClimatica
